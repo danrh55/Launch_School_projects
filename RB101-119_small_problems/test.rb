ed99220806b1f156968724b5fde073d79102dcfa
Eric Cho <ekc7590@gmail.com>
2024-02-06 14:18:34 +0900

    ex questions done

--- RB101-119_small_problems/test.rb
+++ RB101-119_small_problems/test.rb
@@ -1,43 +1,90 @@
-# p format('(%d) %d-%d', 403, 402, 1719)
-# p sprintf('%-d', 100) # => "+64"
+# # Given 2 two arguments of an array of numbers and a number, 
+#find all the pairs that results in the forming a triangle with the number.  - Eric
 
-# # Further Exploration
-# WORDS = %w(zero one two three four five six seven eight nine)
-# DIGITS = ('0'..'9')
+#  Note: To determine if three numbers form a triangle you have touse the Triangle Inequality Theorem, 
+#which states that the sum of two side lengths of a triangle is always greater than the third side. 
+#If this is true for all three combinations of added side lengths, then you will have a triangle.
 
-# WORD_TO_DIGIT = WORDS.zip(DIGITS).to_h
-
-# def word_to_digit2(words)
-#   number_word_matches = 'zero|one|two|three|four|five|six|seven|eight|nine'
-#   matching_regex = /\b('zero|one|two|three|four|five|six|seven|eight|nine'})\b (\s(?=('zero|one|two|three|four|five|six|seven|eight|nine'})))*/xi
-
-#   words.gsub!(matching_regex) do |number_word_match|
-#     WORD_TO_DIGIT[number_word_match.downcase.strip]
-#   end
-#   words.gsub!(/(\d{3})(\d{3})(\d{3})/, '(\1) \2-\3') # formatting for phone numbers
-
-#   words
-# end
+# Test cases
+=begin
+puts find_triangle_pairs([2, 3, 4, 5], 7).inspect # [ [ 3, 5 ], [ 4, 5 ] ]
+puts find_triangle_pairs([1, 2, 3, 4], 10).inspect # []
+puts find_triangle_pairs([7, 10, 12, 15], 9).inspect # [ [ 7, 10 ], [ 7, 12 ], [ 7, 15 ], [ 10, 12 ], [ 10, 15 ], [ 12, 15 ] ]
+puts find_triangle_pairs([8, 2, 5, 6, 3], 4).inspect # [ [ 8, 5 ], [ 8, 6 ], [ 2, 5 ], [ 2, 3 ], [ 5, 6 ], [ 5, 3 ], [ 6, 3 ] ]
 
 
-# puts word_to_digit2('Please call me at four one six five five five one two three four between ' \
-#                     'eight AM and six PM. Thanks.') == 'Please call me at (416) 555-1234 between 8 AM and 6 PM. Thanks.'
+P:
+input: array and integer
+output: nested array of pairs
 
-# puts word_to_digit2('Please call me at FIVE FIVE FIVE One two three Four five six seven. ' \
-#                     'Thanks, Agent 5 2 0') == 'Please call me at (555) 123-4567. Thanks, Agent 5 2 0'
-# puts word_to_digit2('one Two THree FOUr FIVE SIX sEVEN eiGHT ninE zero hi zero hi one two three 1 2 3') ==
-#      '(123) 456-7890 hi 0 hi 123 1 2 3'
-# puts word_to_digit2('Please call me at four one six five five five one two three four between ' \
-#                     'eight AM and six PM. Take none of the freight eighty. Thanks.') ==
-#      'Please call me at (416) 555-1234 between 8 AM and 6 PM. Take none of the freight eighty. Thanks.'
+conditions:
+- get all combination of pairs
+- check if all combination of the three numbers matches triangle inequality theorem
+what will be true:
+- the thir comparison number will always be the same
 
-string = IO.read('example.txt')
-sentences = string.split(/(?<![\.]\w)[\.!?]\s\W/).map {|sentence| sentence.strip}
-longest = ['', 0]
-sentences.each do |sentence|
-  if sentence.split(' ').size > longest[1]
-    longest[0] = sentence
-    longest[1] = sentence.split(' ').size
+implicit:
+- return empty array if no match. 
+
+D:
+array
+
+A:
+initalize triangle pairs array  = []
+
+get_all_combinations method
+
+iterate through each element with index
+  iterate through the index + 1 value element until the last element
+    push each pair to the triangle pairs array
+  return the unique triangle pairs array
+
+triangle? method
+
+
+=end
+
+def all_comb(arr)
+  triangle_pairs = []
+  arr.each_with_index do |ele, index|
+    arr[index+1..-1].each do |ele2|
+      triangle_pairs = triangle_pairs.push([ele, ele2])
+    end
+  end
+  triangle_pairs.uniq
+end
+
+def triangle?(pair, num)
+  pair.sum > num
+end
+
+def find_triangle_pairs(arr, num)
+  all_comb = all_comb(arr)
+  
+  all_comb.select do |pair|
+    pair if triangle?(pair, num)
   end
 end
-p longest
+
+puts find_triangle_pairs([2, 3, 4, 5], 7).inspect # [ [ 3, 5 ], [ 4, 5 ] ]
+puts find_triangle_pairs([1, 2, 3, 4], 10).inspect # []
+puts find_triangle_pairs([7, 10, 12, 15], 9).inspect # [ [ 7, 10 ], [ 7, 12 ], [ 7, 15 ], [ 10, 12 ], [ 10, 15 ], [ 12, 15 ] ]
+puts find_triangle_pairs([8, 2, 5, 6, 3], 4).inspect # [ [ 8, 5 ], [ 8, 6 ], [ 2, 5 ], [ 2, 3 ], [ 5, 6 ], [ 5, 3 ], [ 6, 3 ] ]
+
+def find_triangle_pairs(numbers, fixed_number)
+  valid_pairs = []
+
+  # Helper function to check if three sides can form a triangle
+  def can_form_triangle(a, b, c)
+    a + b > c && a + c > b && b + c > a
+  end
+
+  # Iterate through the array to check each pair with the fixed number
+  numbers.combination(2).each do |pair|
+    if can_form_triangle(pair[0], pair[1], fixed_number)
+      valid_pairs.push(pair)
+    end
+  end
+
+  valid_pairs
+end
